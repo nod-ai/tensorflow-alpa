1ed4ea0d39b068c686c65927805424b946de8cdf
Kyle Herndon <kyle@nod-labs.com>
2022-07-13 05:52:10 +0000

    Serialized mesh data using proto

--- tensorflow/compiler/xla/service/spmd/record_hlo.cc
+++ tensorflow/compiler/xla/service/spmd/record_hlo.cc
@@ -1,10 +1,12 @@
 #include "tensorflow/compiler/xla/service/spmd/record_hlo.h"
 
+#include <iostream>
 #include <fstream>
 
 #include "pybind11/numpy.h"
 #include "pybind11/stl.h"
 #include "tensorflow/compiler/xla/service/pass_context.h"
+#include "tensorflow/compiler/xla/service/spmd/mesh.pb.h"
 
 namespace xla {
 namespace spmd {
@@ -20,7 +22,7 @@ StatusOr<bool> RecordHLO::Run(HloModule* module) {
 	  pass_context::GetDoubleVector("auto_sharding::device_mesh_alpha");
   std::vector<double> mesh_beta =
 	  pass_context::GetDoubleVector("auto_sharding::device_mesh_beta");
-  
+  std::cout << "hello!" << std::endl; 
   std::string serialized_module;
   if (!module->ToProto().SerializeToString(&serialized_module)) {
     return Unknown("Failed to serialize the HloModuleProto.");
@@ -33,32 +35,21 @@ StatusOr<bool> RecordHLO::Run(HloModule* module) {
   module_file << serialized_module;
   module_file.close();
 
-  std::string serialized_mesh;
-  serialized_mesh += "Mesh Shape\n";
-  for (int64_t shape : device_mesh.dimensions()) {
-    serialized_mesh += std::to_string(shape) + ", ";
+  nod::Mesh mesh;
+  for (int64_t id : device_mesh.dimensions()) {
+    mesh.add_mesh_ids(id);
   }
-  serialized_mesh += "\n";
-
-  serialized_mesh += "Mesh Alpha\n";
   for (double a : mesh_alpha) {
-    int64_t value = *reinterpret_cast<int64_t*>(&a);
-    serialized_mesh += std::to_string(value) + ", ";
+    mesh.add_alpha(a);
   }
-  serialized_mesh += "\n";
-  
-  serialized_mesh += "Mesh Beta\n";
   for (double b : mesh_beta) {
-    int64_t value = *reinterpret_cast<int64_t*>(&b);
-    serialized_mesh += std::to_string(value) + ", ";
+    mesh.add_beta(b);
   }
-  serialized_mesh += "\n";
-    
   std::ofstream mesh_file(device_mesh_filepath_);
   if(!mesh_file) {
     return Unknown("Failed to open file");
   }
-  mesh_file << serialized_mesh;
+  mesh.SerializeToOstream(&mesh_file);
   mesh_file.close();
 
   return true;
